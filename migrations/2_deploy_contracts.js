5318ea092a2c9a13d274ea56c1c76df70b5ea9e8
장 윤기 <jangyungi@gmail.com>
2018-02-09 15:59:10 +0900

    Update Migration for test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
+/* Test Migration*/
 var Card = artifacts.require("CryptoSagaCard");
 var Presale = artifacts.require("Presale");
 var Gold = artifacts.require("Gold");
 var Hero = artifacts.require("CryptoSagaHero");
 var CardSwap = artifacts.require("CryptoSagaCardSwapVer1");
+var Dungeon = artifacts.require("ExampleDungeon");
 
 module.exports = function(deployer) {
-  var card, gold, hero, cardSwap;
+  var card, gold, hero, cardSwap, dungeon;
   deployer.then(function() {
     return deployer.deploy(Card);
   }).then(function() {
@@ -29,9 +31,24 @@ module.exports = function(deployer) {
     gold = instance;
     return gold.grantAccessMint(Hero.address);
   }).then(function() {
+    return gold.mint(Hero.address, 10000000000000000000000000);
+  }).then(function() {
     return Hero.deployed();
   }).then(function(instance) {
     hero = instance;
     return hero.grantAccessMint(CardSwap.address);
-  });
+  }).then(function() {
+    return hero.defineType("Farmer", 0, 0, 56, 1, 15, 1, [10, 22, 8, 15, 25], [1, 2, 1, 1, 2], [1, 3, 1, 2, 3]);
+  }).then(function() {
+    return deployer.deploy(Dungeon, Hero.address);
+  }).then(function() {
+    return hero.grantAccessDeposit(Dungeon.address);
+  }).then(function() {
+    return hero.grantAccessDeploy(Dungeon.address);
+  })
 };
+/**/
+
+/* Mainnet Migration*/
+
+/**/
